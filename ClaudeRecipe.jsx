ac70b471f02ba952cb7e8aaf156aa0f9962d3a10
kostiantynkovalchuk <kostiantynkovalchuk32@gmail.com>
2025-03-24 23:17:21 +0100

    spinner added

--- ClaudeRecipe.jsx
+++ ClaudeRecipe.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 
 export default function ClaudeRecipe({ recipe, loading }) {
-  console.log("Recipe content:", recipe); // Log the recipe content for debugging
-
   return (
     <section className="suggested-recipe-container">
       {loading ? (
